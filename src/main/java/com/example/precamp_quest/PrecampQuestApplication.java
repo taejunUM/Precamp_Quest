9d90ecab7cb4898ccd2e8acfbdb7aa3712d738ea
taejunUM <utj0411@naver.com>
2026-01-28 00:35:27 +0900

    Create Project

---
+++ src/main/java/com/example/precamp_quest/PrecampQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.precamp_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecampQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecampQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/precamp_quest/PrecampQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.precamp_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecampQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
